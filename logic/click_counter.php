d5757e4d9f2223fdeb27fef6f3d673a658fd0127
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2025-11-06 12:49:19 +0100

    save click session

--- logic/click_counter.php
+++ logic/click_counter.php
@@ -1,4 +1,14 @@
 <?php
 
+session_start();
+
+if (!isset($_SESSION["counter"])) {
+    $_SESSION["counter"] = 1;
+} else {
+    $counter = $_SESSION["counter"];
+    $counter++;
+    $_SESSION["counter"] = $counter;
+}
+
 // Navigate back to progress page
 header("Location: ../progress.php");
--- progress.php
+++ progress.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +9,8 @@
     <?php require_once "includes/nav.php"; ?>
 
     <main class="container">
-        
+        <a href="logic/click_counter.php">Click</a>     
+        <span><?php echo $_SESSION["counter"]; ?></span>
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
